c7d3ead984fa1a18cde143a6d410a1466c89f294
Nikos Vasilakis <nikos@vasilak.is>
2021-04-02 07:08:02 -0400

    Update setup.sh

--- evaluation/benchmarks/oneliners/input/setup.sh
+++ evaluation/benchmarks/oneliners/input/setup.sh
@@ -1,31 +1,74 @@
 #!/bin/bash
 
-set -e
+# #Check that we are in the appropriate directory where setup.sh is
+# #https://stackoverflow.com/a/246128
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# echo "changing to $DIR to run setup.sh"
+# cd $DIR
 
-# call the script with its absolute name
-cd $(dirname $0)
+PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
-curl 'http://ndr.md/data/dummy/1G.txt' > 1G.txt
-curl 'http://ndr.md/data/dummy/1M.txt' > 1M.txt
-# download wamerican-insane dictionary and sort according to machine
-curl 'http://ndr.md/data/dummy/dict.txt' | sort > dict.txt
 
-if [ "$#" -eq 1 ] && [ "$1" = "--full" ]; then
-  echo Generting full-size inputs
-  # FIXME PR: Do we need all of them?
+# another solution for capturing HTTP status code
+# https://superuser.com/a/590170
 
-  touch 3G.txt
-  for (( i = 0; i < 3; i++ )); do
-    cat 1G.txt >> 3G.txt
-  done
+if [[ "$1" == "-c" ]]; then
+  rm -f 1M.txt all_cmds.txt words sorted_words 10M.txt
+  exit
+fi
 
-  touch 10G.txt
+if [ ! -f ./1M.txt ]; then
+  curl -sf 'http://ndr.md/data/dummy/1M.txt' > 1M.txt
+  if [ $? -ne 0 ]; then
+    curl -sf 'http://www.gutenberg.org/files/2600/2600-0.txt' | head -c 1M > 1M.txt
+    if [ $? -ne 0 ]; then
+      echo 'cannot find 1M.txt -- please contact the developers of pash'
+      exit 1
+    fi
+  fi
+  "$PASH_TOP/scripts/append_nl_if_not.sh" ./1M.txt
+fi
+
+if [ ! -f ./all_cmds.txt ]; then
+  curl -sf 'http://ndr.md/data/dummy/all_cmds.txt' > all_cmds.txt
+  if [ $? -ne 0 ]; then
+    # This should be OK for tests, no need for abort
+    ls /usr/bin/* > all_cmds.txt
+  fi
+  "$PASH_TOP/scripts/append_nl_if_not.sh" ./all_cmds.txt
+fi
+
+if [ ! -f ./words ]; then
+  curl -sf 'http://ndr.md/data/dummy/words' > words
+  if [ $? -ne 0 ]; then
+    if [ $(uname) = 'Darwin' ]; then
+      # On OSX, for development
+      cp /usr/share/dict/web2 words
+      if [ $? -ne 0 ]; then
+        echo 'cannot find dict file -- please contact the pash developers'
+        exit 1
+      fi
+    else
+      # On Linux and Debian, for experiments
+      # apt install wamerican-insane
+      cp /usr/share/dict/words words
+      if [ $? -ne 0 ]; then
+        echo 'cannot find dict file -- please contact the pash developers'
+        exit 1
+      fi
+    fi
+  fi
+  "$PASH_TOP/scripts/append_nl_if_not.sh" words
+fi
+
+if [ ! -f ./10M.txt ]; then
+  touch 10M.txt
   for (( i = 0; i < 10; i++ )); do
-    cat 1G.txt >> 10G.txt
+    cat 1M.txt >> 10M.txt
   done
+fi
 
-  touch 100G.txt
-  for (( i = 0; i < 10; i++ )); do
-    cat 10G.txt >> 100G.txt
-  done
+## Re-sort words for this machine
+if [ ! -f ./sorted_words ]; then
+  sort words > sorted_words
 fi
--- evaluation/benchmarks/poets/input/setup.sh
+++ evaluation/benchmarks/poets/input/setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# FIXME: when compression is ready, apply to all PG books
 
-# wget ndr.md/data/pg.xz | tar -xf 
-mkdir -p $PASH_TOP/evaluation/benchmarks/poets/input/output
 if [[ "$1" == "-c" ]]; then
-    rm -f genesis exodus
+    rm -f genesis exodus pg/
     exit
 fi
+
+# FIXME: when compression is ready, apply to all PG books
+# decompression takes about 10 minutes
+# if [ ! -e ./pg ]; then
+#   curl -f ndr.md/data/pg.xz | tar -xJ
+#   if [ $? -ne 0 ]; then
+#     echo 'Cannot extract project gutenberg -- please contact the pash developers'
+#     exit 1
+#   fi
+# fi
 
 if [ ! -f ./genesis ]; then
   curl -sf http://www.gutenberg.org/cache/epub/8001/pg8001.txt > genesis
