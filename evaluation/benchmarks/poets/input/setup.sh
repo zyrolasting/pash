cab52977feff93d3ce9004589a683c6c5c4ab7a1
Nikos Vasilakis <nikos@vasilak.is>
2021-04-02 07:17:11 -0400

    Minor correction

--- evaluation/benchmarks/poets/input/setup.sh
+++ evaluation/benchmarks/poets/input/setup.sh
@@ -2,16 +2,21 @@
 
 
 if [[ "$1" == "-c" ]]; then
-    rm -f genesis exodus pg/
+    rm -f genesis pg.tar.xz exodus pg/
     exit
 fi
 
 # FIXME: when compression is ready, apply to all PG books
 # decompression takes about 10 minutes
 # if [ ! -e ./pg ]; then
-#   curl -f ndr.md/data/pg.xz | tar -xJ
+#   wget ndr.md/data/pg.tar.xz 
 #   if [ $? -ne 0 ]; then
-#     echo 'Cannot extract project gutenberg -- please contact the pash developers'
+#     echo 'Cannot find pg.tar.xz -- please contact the pash developers'
+#     exit 1
+#   fi
+#  tar -xJ -f pg.tar.xz
+#   if [ $? -ne 0 ]; then
+#     echo 'Cannot extract pg.tar.xz -- please contact the pash developers'
 #     exit 1
 #   fi
 # fi
